aa53b5efbd8dd0a5c42d1f43a11723aa47a24bae
Diogo Bento <diogo.bento@standardbank.co.mz>
2022-12-23 17:13:06 -0100

    feat: First unit test.

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,7 +1,37 @@
-import { CartComponent } from "./cart.component"
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CartComponent } from "./cart.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { BookService } from "../../services/book.service";
 
 describe('Cart component', () => {
 
-  let component: CartComponent
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
 
-})
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [
+        CartComponent
+      ],
+      providers: [
+        BookService
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
