774b00e8133a4e289cf43d5575add99e94f1e619
Diogo Bento <diogo.bento@standardbank.co.mz>
2022-12-28 14:53:19 -0100

    feat: Test coverage for Home Component. (Subscription Test)

---
+++ src/app/pages/home/home.component.spec.ts
@@ -0,0 +1,78 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Observable, of } from "rxjs";
+import { Book } from "src/app/models/book.model";
+import { BookService } from "../../services/book.service";
+import { HomeComponent } from "./home.component";
+
+const listBook: Book[] = [
+  {
+    name: '',
+    author: '',
+    isbn: '',
+    price: 15,
+    amount: 2
+  },
+  {
+    name: '',
+    author: '',
+    isbn: '',
+    price: 20,
+    amount: 1
+  },
+  {
+    name: '',
+    author: '',
+    isbn: '',
+    price: 8,
+    amount: 7
+  },
+]
+
+describe('Home component', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [
+        HomeComponent
+      ],
+      providers: [
+        BookService
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+it('should create', () => {
+  expect(component).toBeTruthy();
+});
+
+
+/*public getBooks(): void {
+  this.bookService.getBooks().pipe(take(1)).subscribe((resp: Book[]) => {
+    this.listBook = resp;
+  });
+}*/
+
+it('getBook get books from the subscription', () => {
+  const bookService = fixture.debugElement.injector.get(BookService);
+  const spy1 = jest.spyOn(bookService, 'getBooks').mockReturnValueOnce( of(listBook) );
+  component.getBooks();
+  expect(spy1).toHaveBeenCalledTimes(1);
+  expect(component.listBook.length).toBe(3);
+  expect(component.listBook).toEqual(listBook);
+});
+
+});
